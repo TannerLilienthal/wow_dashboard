4fb80b1d1aaed1e456d5a23536bfe7dddef17539
Tanner Lilienthal <89283339+TannerLilienthal@users.noreply.github.com>
2022-12-04 14:15:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,35 +9,41 @@ client_key = '?api_key=bc47c55038353c9ea9a5f32148822099'
 
 def zone_request():
     return requests.get(api_url + 'zones' + client_key).json()
+    # params:   none
     # returns:  list[] > dict{id, name, frozen, encounters, brackets, partitions}
 
 
 def class_request():
     return requests.get(api_url + 'classes' + client_key).json()
+    # params:   none
     # returns:  list > dict{id, name, specs[list > dict{id name}]}
 
 
 def encounter_ranking_request(encounter_id):
     return requests.get(api_url + 'rankings/encounter/' + encounter_id + client_key).json()
-    # page, hasMorePages, count, rankings[name, class, spec, total, duration, startTime, fightID, reportID, guildName,
-    # serverName, regionName, hidden, itemLevel, faction, size
+    # params:   encounterID, metric, size, difficulty, partition, class, spec, bracket, server, region, page, filter, bool(includeCombtantInfo)
+    # returns:  dict {page, hasMorePages, count, rankings{name, class, spec, total, duration, startTime, fightID, reportID, guildName,
+    #           serverName, regionName, hidden, itemLevel, faction, size}}
 
 
 def character_ranking_request(character_name):
     return requests.get(api_url + 'rankings/character/' + character_name + '/Eranikus/US' + client_key).json()
-    # encounterID, encounterName, class, spec, rank, outOf, duration, startTime, reportID, fightID, difficulty, size,
-    # characterID, characterName, server, percentile, ilvlKeyOrPatch, total, estimated
+    # params:   characterName
+    # returns list > dict {encounterID, encounterName, class, spec, rank, outOf, duration, startTime, reportID, fightID, 
+    # difficulty, size, characterID, characterName, server, percentile, ilvlKeyOrPatch, total, estimated}
 
 
 def parse_request(character_name):
     return requests.get(api_url + 'parses/character/' + character_name + '/Eranikus/US' + client_key).json()
-    # encounterID, encounterName, class, spec, rank, outOf, duration, startTime, reportID, fightID, difficulty, size,
-    # characterID, characterName, server, percentile, ilvlKeyOrPatch, total, estimated
+    # params:   characterName
+    # returns:  list > dict {encounterID, encounterName, class, spec, rank, outOf, duration, startTime, reportID, fightID, 
+    # difficulty, size, characterID, characterName, server, percentile, ilvlKeyOrPatch, total, estimated}
 
 
-def guild_report_reuqest(guild_name):
+def guild_report_request(guild_name):
     return requests.get(api_url + 'reports/guild/' + guild_name + '/Eranikus/US' + client_key).json()
-    # id, title, owner, start, end, zone
+    # params: guildName
+    # returns: id, title, owner, start, end, zone
 
 
 def fight_request(code):
@@ -70,4 +76,4 @@ def table_request(view, code, params):
     #           cutoff, encounter, wipes, difficulty, filter
 
 
-character_ranking_request('Duddlez')
+print(encounter_ranking_request(str(101120)).get('rankings'))
